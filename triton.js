1ffaf0786cb837366c34b9530eeb88f112b1ef2b
aditya parab <adparab97@gmail.com>
2019-03-16 17:48:30 -0400

    cc

---
+++ triton.js
@@ -0,0 +1,15 @@
+const name = 'Adp'
+const age= 22
+const hasHobbies = true
+age = 23
+
+age = 24
+function triton (n,a,h){
+    return ("the user name is "+n+" and his age is "+a+" and the the fact that he is the best ever is: "+h)
+
+}
+
+
+
+
+console.log(triton(name,age,hasHobbies))s
