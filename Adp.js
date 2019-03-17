47ee4c06e97c8d64e62b571bb29a427a60ff1b59
aditya parab <adparab97@gmail.com>
2019-03-17 11:26:23 -0400

    hi

--- Adp.js
+++ Adp.js
@@ -1,3 +1,7 @@
-const fs = require("fs")
+// const fs = require("fs")
 
-fs.writeFileSync("hello.txt","Hello from Master Adp") 
+// // fs.writeFileSync("hello.txt","Hello from Master Adp") 
+// let a = "XXX"
+// let b="YYY"
+// const hobbies = ['1', '2', '3']
+// console.log(hobbies.map( => ("number is: " + func)))
--- triton.js
+++ triton.js
@@ -1,15 +1,8 @@
-const name = 'Adp'
-const age= 22
-const hasHobbies = true
-age = 23
-
-age = 24
-function triton (n,a,h){
-    return ("the user name is "+n+" and his age is "+a+" and the the fact that he is the best ever is: "+h)
-
-}
-
-
-
-
-console.log(triton(name,age,hasHobbies))s
+const person = {
+    name : "Aditya",
+    age : 21,
+    greet(){
+        console.log("Greetings Stranger. I am " + this.name)
+        
+    }
+}   
